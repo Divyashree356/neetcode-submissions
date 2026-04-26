d464bf8f19115b15ea38726c8c0ebd3c45a1c191
Divyashree Agrawal <64089485+Divyashree356@users.noreply.github.com>
2026-04-26 12:45:38 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        int i =0;
+        int j = s.length() -1;
+
+        // String s = str.tLowerCase();
+        System.out.println(i);
+        System.out.println(j);
+        
+        while(i<j){
+        System.out.println("here");
+        
+        char c1 = s.charAt(i);
+        c1 = Character.toLowerCase(c1);
+        char c2 = s.charAt(j);
+        c2 = Character.toLowerCase(c2);
+
+        System.out.println(c1);
+        System.out.println(c2);
+
+        if (!((c1 >= 'a' && c1 <= 'z') || (c1 >= '0' && c1 <= '9'))) {
+            i++;
+            continue;
+        }
+
+        if (!((c2 >= 'a' && c2 <= 'z' ) || (c2 >= '0' && c2 <= '9'))){
+            j--;
+            continue;
+        }
+
+        if (c1 != c2) return false;
+        else {
+            i++;
+            j--;
+        }
+    }
+return true;
+}
+}
