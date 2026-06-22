24a4933666ae384091e08bc05d25efeda7feae51
Divyashree Agrawal <64089485+Divyashree356@users.noreply.github.com>
2026-06-22 12:32:48 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+            int a = 1;
+            int b= 2;
+            int res = 0;
+            if (n <= 2) return n;      
+            for(int i =3; i <= n; i++){
+                res = a+b;
+                a = b;
+                b = res;
+            }
+            return res;
+    }
+}
